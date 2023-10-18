83992792086065e907b31820b6edc609fc55a7e5
tweaks4roothide <9qq9kxs0@duck.com>
2023-10-18 06:26:00 +0300

    update

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@
 dpkg-scanpackages ./debfiles/ > ./Packages;
 #sed -i -e '/^SHA/d' ./Packages;
 bzip2 -c9k ./Packages > ./Packages.bz2;
-printf "Origin: Tweaks4Roothide's Repo\nLabel: Tweaks4Roothide\nSuite: stable\nVersion: 1.0\nCodename: Tweaks4Roothide\nArchitecture: iphoneos-arm64e\nComponents: main\nDescription: Tweaks4Roothide's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+printf "Origin: Tweaks4Roothide's Repo\nLabel: Tweaks4Roothide\nSuite: stable\nVersion: 1.0\nCodename: Tweaks4Roothide\nArchitecture: iphoneos-arm64e\nComponents: main\nDescription: Tweaks4Roothide's\nIcon: file://icon.png 
+
+Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
 exit 0;
